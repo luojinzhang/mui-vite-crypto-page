013863ca4eaedee0c6cb7946862fa58ce82b5d7d
Cam Chuong Lac <laccamchuong@gmail.com>
2024-12-31 17:23:35 +1100

    feat: init mui-vite crypto page project

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import * as React from "react";
+import * as ReactDOM from "react-dom/client";
+import { ThemeProvider } from "@mui/material/styles";
+import { CssBaseline } from "@mui/material";
+import theme from "./theme";
+import App from "./App";
+import { Provider } from "react-redux";
+import { store } from "./redux/store";
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </ThemeProvider>
+  </React.StrictMode>
+);
