1d78225cbc2f3487c33cb89d87744c658280c01d
Cam Chuong Lac <laccamchuong@gmail.com>
2025-01-02 12:44:51 +1100

    feat: add eslint configuration

---
+++ .eslintrc.cjs
@@ -0,0 +1,16 @@
+module.exports = {
+  extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended", "plugin:prettier/recommended"],
+  env: {
+    es6: true,
+    browser: true,
+  },
+  plugins: ["@typescript-eslint", "prettier"],
+  parserOptions: {
+    parser: "@typescript-eslint/parser",
+  },
+  rules: {
+    "@typescript-eslint/no-namespace": "off",
+    "@typescript-eslint/no-explicit-any": "warn",
+    "@typescript-eslint/no-unused-vars": "warn",
+  },
+};
